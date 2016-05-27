1ce2b7cc3d9d4313049c04a9f2c56924febc3103
Matthew Baptist <mbaptist34@gmail.com>
2016-05-27 16:21:33 -0400

    Added function prototypes for some matrix operations and implemented matrix add, sub, and mul.

--- math3.c
+++ math3.c
@@ -8,6 +8,8 @@
 
 #include "math3.h"
 
+/* Vector Operations */
+
 float dot(int dim, float[] vec1, float[] vec2){
   
   float accum = 0;
@@ -49,3 +51,49 @@ void scale(int dim, float[] vec, float scalar, float[] rslt) {
     rslt[i] = vec[i] * scalar;
   }
 }
+
+/* Matrix Operations */
+
+
+/* Component wise addition of the two matrices, stored in rslt */
+void add(int dim, float[] mat1, float[] mat2, float[] rslt) {
+  
+  for(int i = 0; i < dim; i++) {
+    for(int j = 0; j < dim; j++) {
+      rslt[i][j] = mat1[i][j] + mat2[i][j];
+    }
+  }
+}
+
+/* Component wise subtraction of the two matrices, stored in rslt */
+void sub(int dim, float[] mat1, float[] mat2, float[] rslt) {
+  
+  for(int i = 0; i < dim; i++) {
+    for(int j = 0; j < dim; j++) {
+      rslt[i][j] = mat1[i][j] - mat2[i][j];
+    }
+  }
+}
+
+/* Compute the matrix multiplication of the two matrices, store in rslt */
+void mul(int dim, float[] mat1, float[] mat2, float[] rslt) {
+  
+  float entry = 0;
+  for(int i = 0; i < dim; i++){
+    for(int j = 0; j < dim; j++){
+      entry = 0
+      for(int k = 0; k < dim; k++){
+        entry += mat1[i][k] * mat2[k][j]
+      }
+      rslt[i][j] = entry;
+    }
+  }
+}
+
+/* Compute the determinant of the matrix */
+float det(int dim, float[] mat);
+
+/* Find the inverse of the square matrix if one exists */
+void inv(int dim, float[] mat, float[] rslt);
+
+
--- math3.h
+++ math3.h
@@ -11,6 +11,15 @@
 
 #include <stdio.h>
 
+/*
+    Vector Operations:
+    -dot
+    -add
+    -sub
+    -norm
+    -scale
+ */
+
 /* Compute the dot product of the vectors */
 float dot(int dim, float[] vec1, float[] vec2);
 
@@ -26,4 +35,30 @@ void norm(int dim, float[] vec, float[] rslt);
 /* Scale vec by scalar */
 void scale(int dim, float[] vec, float scalar, float[] rslt);
 
+/*
+  Note: These ops are only defined on square matrices.
+  Matrix Operations:
+  -add
+  -sub
+  -mul
+  -det
+  -inv
+ */
+
+/* Component wise addition of the two matrices, stored in rslt */
+void add(int dim, float[] mat1, float[] mat2, float[] rslt);
+
+/* Component wise subtraction of the two matrices, stored in rslt */
+void sub(int dim, float[] mat1, float[] mat2, float[] rslt);
+
+/* Compute the matrix multiplication of the two matrices, store in rslt */
+void mul(int dim, float[] mat1, float[] mat2, float[] rslt);
+
+/* Compute the determinant of the matrix */
+float det(int dim, float[] mat);
+
+/* Find the inverse of the square matrix if one exists */
+void inv(int dim, float[] mat, float[] rslt);
+
+/* Matrix multiplication for two sq
 #endif /* math3_h */
